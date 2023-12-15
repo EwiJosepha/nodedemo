2f691826a7f38bbee1492ade2f83a635485d8cc2
ewi josepha <ewijosepha@gmail.com>
2023-12-15 08:42:49 +0100

    uploading css file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http')
-const {readFileSync} = require("fs")
+const { readFileSync } = require("fs")
 
-const homepage = readFileSync('./navbar-app/index.html')
+const homepage = readFileSync('./navbar-app/indexx.html')
 const styles = readFileSync('./navbar-app/styles.css')
 const scriptt = readFileSync('./navbar-app/browserapp.js')
 const server = http.createServer((req, res) => {
@@ -11,18 +11,26 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" })
     res.write(homepage)
     res.end()
-    res.writeHead(200, { "content-type": "text/styles" })
-    res.write(styles)
-    res.end()
-    res.writeHead(200, { "content-type": "text/script" })
-    res.write(scriptt)
-    res.end()
-  } else if(url === "/about") {
+  }
+
+ 
+  else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" })
-    res.write(`<h1>about page today</h1>`)
+    res.write(homepage)
     res.end()
 
-  }else{
+  }else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" })
+    res.write(styles)
+    res.end()
+  }
+
+   else if (url === "/browserapp") {
+    res.writeHead(200, { "content-type": "text/javasript" })
+    res.write(scriptt)
+    res.end()
+  }
+   else {
     res.writeHead(404, { "content-type": "text/html" })
     res.write(`<h1>Page not found</h1>`)
     res.end()
